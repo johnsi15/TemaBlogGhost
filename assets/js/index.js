7953f09f50377e3c3c1db416fce6a7aad8d2d6e0
john andrey <andrey_201015@hotmail.com>
2016-04-21 18:47:50 -0500

    Subiendo template

---
+++ assets/js/index.js
@@ -0,0 +1,85 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/* globals jQuery, document */
+(function ($, undefined) {
+    "use strict";
+
+    var $document = $(document);
+
+    $document.ready(function () {
+
+        var $postContent = $(".post-content");
+        $postContent.fitVids();
+
+        $(".scroll-down").arctic_scroll();
+
+        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+            e.preventDefault();
+            $("body").toggleClass("nav-opened nav-closed");
+        });
+
+    });
+
+    var contador = 1;
+    $('.bt-menu').click(function(e){
+          e.preventDefault();
+        if(contador == 1){
+            $('.nav').animate({
+                right: '0'
+            });
+            contador = 0;
+        }else {
+            contador = 1;
+            $('.nav').animate({
+                right: '-100%'
+            });
+        }
+    });
+
+    // Arctic Scroll by Paul Adam Davis
+    // https://github.com/PaulAdamDavis/Arctic-Scroll
+    $.fn.arctic_scroll = function (options) {
+
+        var defaults = {
+            elem: $(this),
+            speed: 500
+        },
+
+        allOptions = $.extend(defaults, options);
+
+        allOptions.elem.click(function (event) {
+            event.preventDefault();
+            var $this = $(this),
+                $htmlBody = $('html, body'),
+                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+                toMove;
+
+            if (offset) {
+                toMove = parseInt(offset);
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+            } else if (position) {
+                toMove = parseInt(position);
+                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+            } else {
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+            }
+        });
+
+    };
+
+
+    /*var menu = $('.menu');
+    var cuadro_offset = menu.offset();*/
+    var posicion = 100;
+    $(window).on('scroll', function() {
+        if($(window).scrollTop() > posicion) {
+            $(".menu").css("background", "rgba(28,28,28,11)");
+        } else {
+            $(".menu").css("background", "linear-gradient(to bottom, rgba(0,0,0,0.2) 0%,rgba(0,0,0,0) 100%)");
+        }
+    });
+
+})(jQuery);
